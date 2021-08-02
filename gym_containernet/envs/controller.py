b2624d716e3a113c5f5b9f491a5ac0a85a3e61d6
pmsdOliveira <pmsd.oliveira@campus.fct.unl.pt>
2021-08-02 18:07:07 +0100

    Controller now only recomputes paths when there is a change in BW

--- gym_containernet/envs/controller.py
+++ gym_containernet/envs/controller.py
@@ -7,7 +7,7 @@ from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.topology import event
-from ryu.topology.api import get_switch, get_link
+from ryu.topology.api import get_switch
 from ryu.controller.controller import Datapath
 from ryu.lib import hub
 from ryu.lib.packet import packet, ethernet, ether_types
@@ -18,7 +18,8 @@ import os
 from typing import Any, Dict, List, Tuple, Union
 
 
-UPDATE_PERIOD = 10
+UPDATE_PERIOD = 10  # seconds
+BW_VARIANCE = 5000  # kbits
 
 # Custom types
 EndpointPair = Tuple[int, int]
@@ -92,6 +93,7 @@ class Controller(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args: Tuple, **kwargs: Dict[str, Any]) -> None:
+        os.system("clear")
         super(Controller, self).__init__(*args, **kwargs)
 
         self.host_switch_port: Dict[str, SwitchPortPair]
@@ -101,6 +103,7 @@ class Controller(app_manager.RyuApp):
 
         self.switch_datapath: Dict[int, Datapath] = {}
 
+        self.prev_available_bw: Dict[EndpointPair, float] = defaultdict(lambda: 0.0)
         self.available_bw: Dict[EndpointPair, float] = defaultdict(lambda: 0.0)
         self.used_bw: Dict[EndpointPair, float] = defaultdict(lambda: 0.0)
         self.tx_bytes: Dict[EndpointPair, int] = defaultdict(lambda: 0)
@@ -117,20 +120,19 @@ class Controller(app_manager.RyuApp):
 
     def monitor(self) -> None:
         while True:
-            os.system("clear")
-            print(datetime.now().strftime("%H:%M:%S\n"))
+            print(datetime.now().strftime("\n\n%H:%M:%S\n"))
             for datapath in self.switch_datapath.values():
                 request_stats(datapath)
             hub.sleep(UPDATE_PERIOD)
 
     @set_ev_cls([event.EventSwitchEnter, event.EventSwitchLeave, event.EventPortAdd, event.EventPortDelete, event.EventPortModify,
                  event.EventLinkAdd, event.EventLinkDelete])
-    def _get_topology_data(self, ev):
+    def get_topology_data(self, ev) -> None:
         for switch in get_switch(self.topology_api_app, None):  # get all switches and corresponding datapaths
             self.switch_datapath[switch.dp.id] = switch.dp
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
-    def switch_features_handler(self, ev):  # create table-miss entries
+    def switch_features_handler(self, ev) -> None:  # create table-miss entries
         datapath = ev.msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
@@ -142,12 +144,12 @@ class Controller(app_manager.RyuApp):
         datapath.send_msg(mod)
 
     @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
-    def _port_stats_reply_handler(self, ev):
+    def port_stats_reply_handler(self, ev) -> None:
         msg = ev.msg
         datapath = ev.msg.datapath
         dpid = datapath.id
         self.used_bw, self.available_bw, self.tx_bytes, self.clock = \
-            network_observator.update_bw(msg, self.switch_datapath.keys(), self.adjacency, self.bw, self.available_bw,
+            network_observator.update_bw(msg, list(self.switch_datapath.keys()), self.adjacency, self.bw, self.available_bw,
                                          self.used_bw, self.tx_bytes, self.clock)
         for dst in self.switch_datapath.keys():
             if self.adjacency[dpid, dst]:
@@ -157,18 +159,23 @@ class Controller(app_manager.RyuApp):
         self.updated.append(dpid)
         if len(set(self.updated)) == len(self.switch_datapath.keys()):  # if all switches have updated their bw
             self.updated = []  # reset it for next path calculation
-            self.paths, self.best_path = path_calculator.possible_and_best_paths(self.host_switch_port, self.adjacency,
-                                                                                 self.available_bw)
-            for src in self.host_switch_port.keys():
-                for dst in self.host_switch_port.keys():
-                    if src != dst:
-                        self.switch_flows = install_path(src, dst, self.best_path[src, dst], self.switch_datapath, self.switch_flows)
-                        for path in self.paths[src, dst]:
-                            print("%s -> %s: %s    %s" % (src, dst, path, "IN USE" if path == self.best_path[src, dst] else ""))
-                    print()
+            for (s1, s2), bw in self.available_bw.items():
+                if abs(bw - self.prev_available_bw.get((s1, s2), 0.0)) > 5000:  # only recalculate paths when BW changes > 5000
+                    self.paths, self.best_path = path_calculator.possible_and_best_paths(self.host_switch_port, self.adjacency, self.available_bw)
+                    for src in self.host_switch_port.keys():
+                        for dst in self.host_switch_port.keys():
+                            if src != dst:
+                                self.switch_flows = install_path(src, dst, self.best_path[src, dst], self.switch_datapath, self.switch_flows)
+                                for path in self.paths[src, dst]:
+                                    print("%s -> %s: %s    %s" % (src, dst, path, "IN USE" if path == self.best_path[src, dst] else ""))
+                            print()
+                    self.prev_available_bw = self.available_bw
+                    break
+                else:
+                    self.prev_available_bw[s1, s2] = self.available_bw[s1, s2]
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def _packet_in_handler(self, ev):
+    def packet_in_handler(self, ev) -> None:
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
