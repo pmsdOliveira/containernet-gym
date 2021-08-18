e8ed1ec4e82f783911ef4a1a32fb212a458e55be
pmsdOliveira <pmsd.oliveira@campus.fct.unl.pt>
2021-08-18 12:02:03 +0100

    Added module to test Containernet Environment with random actions

---
+++ gym_containernet/envs/env_test.py
@@ -0,0 +1,19 @@
+import containernet_env
+from random import randint
+import time
+
+episodes = 5
+env = containernet_env.ContainernetEnv()
+time.sleep(5)
+
+for episode in range(1, episodes+1):
+    print("Starting episode...")
+    env.reset()
+    done = False
+    score = 0
+
+    while not done:
+        n_state, reward, done, info = env.step(randint(0, 1))
+        score += reward
+
+    print(f'Episode: {episode}, Score: {score}\n\n')
